7afaa4c81bd5198c63f05bc070afec41a17a876f
Ivan Dev <g.ivan@dizboard.com>
2016-06-15 17:09:59 +0300

    rewrite to php7

--- src/Url/Url.php
+++ src/Url/Url.php
@@ -1,140 +1,156 @@
 <?php
 
-    namespace Dez\Url;
+namespace Dez\Url;
 
-    use Dez\DependencyInjection\Injectable;
-    use Dez\EventDispatcher\Dispatcher as EventDispatcher;
-    use Dez\Http\Request;
-    use Dez\Router\Router;
+use Dez\DependencyInjection\Injectable;
+use Dez\EventDispatcher\Dispatcher as EventDispatcher;
+use Dez\Http\Request;
+use Dez\Router\Router;
+
+/**
+ * @property Request request
+ * @property Router router
+ * @property EventDispatcher eventDispatcher
+ */
+class Url extends Injectable
+{
 
     /**
-     * @property Request request
-     * @property Router router
-     * @property EventDispatcher eventDispatcher
+     * @var string
      */
+    protected $staticPath = '/';
 
-    class Url extends Injectable {
+    /**
+     * @var string
+     */
+    protected $basePath = '/';
 
-        /**
-         * @var string
-         */
-        protected $staticPath   = '/';
-
-        /**
-         * @var string
-         */
-        protected $basePath     = '/';
-
-        /**
-         * @return Request
-         */
-        public function getRequest() {
-            return $this->request;
-        }
-
-        /**
-         * @return Router
-         */
-        public function getRouter() {
-            return $this->router;
-        }
-
-        /**
-         * @return EventDispatcher
-         */
-        public function getEventDispatcher() {
-            return $this->eventDispatcher;
-        }
-
-        /**
-         * @return string
-         */
-        public function getStaticPath() {
-            return $this->staticPath;
-        }
-
-        /**
-         * @param string $staticPath
-         * @return $this
-         */
-        public function setStaticPath($staticPath) {
-            $this->staticPath = $staticPath;
-            return $this;
-        }
-
-        /**
-         * @return string
-         */
-        public function getBasePath() {
-            return $this->basePath;
-        }
-
-        /**
-         * @param string $basePath
-         * @return $this
-         */
-        public function setBasePath( $basePath ) {
-            $this->basePath = $basePath;
-            return $this;
-        }
-
-        /**
-         * @param $macros
-         * @param array $params
-         * @param array $query
-         * @return null|string
-         */
-        public function create( $macros, array $params = [], array $query = [] ) {
-            $builder    = new Builder( $macros, $params, $this->getRouter() );
-            if( $builder->make() ) {
-                return $this->path( $builder->getLink(), $query );
-            } else {
-                return null;
-            }
-        }
-
-        /**
-         * @param string $path
-         * @param array $query
-         * @return string
-         */
-        public function path( $path = '', array $query = [] ) {
-            $path       = ltrim( $path, '/' );
-            $basepath   = rtrim( $this->getBasePath(), '/' );
-            return ( new Uri( "$basepath/$path" ) )->setQueryArray( $query )->local();
-        }
-
-        /**
-         * @param string $path
-         * @param array $query
-         * @return string
-         */
-        public function staticPath( $path = '', array $query = [] ) {
-            $path       = ltrim( $path, '/' );
-            $staticpath = rtrim( $this->getStaticPath(), '/' );
-            return ( new Uri( "$staticpath/$path" ) )->local();
-        }
-
-        /**
-         * @param string $path
-         * @param array $query
-         * @param null|string $fragment
-         * @return string
-         */
-        public function full( $path  = '', array $query = [], $fragment = null ) {
-            $path       = ltrim( $path, '/' );
-            $basepath   = rtrim( $this->getBasePath(), '/' );
-
-            $uri        = ( new Uri( "$basepath/$path" ) )
-                ->setSchema( $this->request->getSchema() )
-                ->setHost( $this->request->getServerHttp( 'host' ) )
-                ->setQueryArray( $query );
-
-            if( $fragment !== null ) {
-                $uri->setFragment( ltrim( $fragment, '#' ) );
-            }
-
-            return $uri->full();
-        }
-
+    /**
+     * @return Request
+     */
+    public function getRequest() : Request
+    {
+        return $this->request;
     }
+
+    /**
+     * @return EventDispatcher
+     */
+    public function getEventDispatcher() : EventDispatcher
+    {
+        return $this->eventDispatcher;
+    }
+
+    /**
+     * @return Router
+     */
+    public function getRouter() : Router
+    {
+        return $this->router;
+    }
+
+    /**
+     * @param string $macros
+     * @param array $params
+     * @param array $query
+     * @return string
+     */
+    public function create(string $macros, array $params = [], array $query = []) : string
+    {
+        $builder = new Builder($macros, $params, $this->getRouter());
+
+        if ($builder->make()) {
+            return $this->path($builder->getLink(), $query);
+        }
+
+        return '';
+    }
+
+    /**
+     * @param string $path
+     * @param array $query
+     * @return string
+     */
+    public function path(string $path, array $query = []) : string
+    {
+        $path = ltrim($path, '/');
+        $basepath = rtrim($this->getBasePath(), '/');
+
+        return (new Uri("$basepath/$path"))->setQueryArray($query)->local();
+    }
+
+    /**
+     * @return string
+     */
+    public function getBasePath() : string
+    {
+        return $this->basePath;
+    }
+
+    /**
+     * @param string $basePath
+     * @return Url
+     */
+    public function setBasePath(string $basePath) : static
+    {
+        $this->basePath = $basePath;
+
+        return $this;
+    }
+
+    /**
+     * @param string $path
+     * @param array $query
+     * @return string
+     */
+    public function staticPath(string $path, array $query = [])
+    {
+        $path = ltrim($path, '/');
+        $staticpath = rtrim($this->getStaticPath(), '/');
+
+        return (new Uri("$staticpath/$path"))->local();
+    }
+
+    /**
+     * @return string
+     */
+    public function getStaticPath() : string
+    {
+        return $this->staticPath;
+    }
+
+    /**
+     * @param string $staticPath
+     * @return Url
+     */
+    public function setStaticPath(string $staticPath) : static
+    {
+        $this->staticPath = $staticPath;
+
+        return $this;
+    }
+
+    /**
+     * @param string $path
+     * @param array $query
+     * @param string $fragment
+     * @return string
+     */
+    public function full(string $path, array $query = [], string $fragment) : string
+    {
+        $path = ltrim($path, '/');
+        $basepath = rtrim($this->getBasePath(), '/');
+
+        $uri = (new Uri("$basepath/$path"))
+            ->setSchema($this->request->getSchema())
+            ->setHost($this->request->getServerHttp('host'))
+            ->setQueryArray($query);
+
+        if ($fragment !== null) {
+            $uri->setFragment(ltrim($fragment, '#'));
+        }
+
+        return $uri->full();
+    }
+
+}
